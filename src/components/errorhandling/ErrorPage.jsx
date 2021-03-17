495f2cce7e6bee794728e99a33508f5601515360
Carina Poell <carrie.poell@gmail.com>
2021-03-17 15:08:52 +0100

    feat: enable user to inspect error with errorstack

--- src/components/errorhandling/ErrorPage.jsx
+++ src/components/errorhandling/ErrorPage.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Accordion, Icon } from 'semantic-ui-react';
+import { withRouter, useHistory } from 'react-router-dom';
 
 import './ErrorPage.css';
 
 // As a basis for this page, I used a template made by Colorlib (https://colorlib.com)
 // and adjusted the coloring to fit with Ospin's brand image.
 
-const ErrorPage = () => {
+const ErrorPage = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  let history = useHistory();
+  const goBack = () => {
+    history.go(-2);
+  };
+
+  const prevLocation = props.history.location.state;
+
+  let errorInfo = null;
+
+  const isOpenHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
+  if (prevLocation) {
+    errorInfo = (
+      <Accordion>
+        <Accordion.Title active onClick={isOpenHandler}>
+          <Icon name="dropdown" />
+          Inspect the error
+        </Accordion.Title>
+        <Accordion.Content active={isOpen}>
+          {prevLocation.errorInfo}
+        </Accordion.Content>
+      </Accordion>
+    );
+  }
+
   return (
     <Container id="error" data-testid="error-container">
       <div className="error">
@@ -18,12 +48,12 @@ const ErrorPage = () => {
             We're experiencing an internal error. Please try again later or
             contact us!
           </p>
-          <a onClick={console.log('goback')}>Go Back</a>
-          <div className="error-info-box">{console.log('placeholder')}</div>
+          <a onClick={{ goBack }}>Go Back</a>
+          <div className="error-info-box">{errorInfo}</div>
         </div>
       </div>
     </Container>
   );
 };
 
-export default ErrorPage;
+export default withRouter(ErrorPage);
